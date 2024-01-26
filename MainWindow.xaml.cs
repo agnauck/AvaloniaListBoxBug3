95a8f95a995c0059d7d241dc38124bd4a4bb7287
Alexander Gnauck <gnauck@gmail.com>
2024-01-26 17:33:18 +0100

    adjust for ScrollToBottom bug

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace AvaloniaListBoxBug
+﻿using Avalonia.Controls.Primitives;
+using Avalonia.Interactivity;
+
+namespace AvaloniaListBoxBug
 {
     using Avalonia;
     using Avalonia.Controls;
@@ -6,15 +9,36 @@
 
     public class MainWindow : Window
     {
+        private readonly ListBox _messageList;
+        private ScrollViewer scroll;
+        
         public MainWindow()
         {
             InitializeComponent();
             DataContext = new MainWindowViewModel();
+            
+            _messageList = this.FindControl<ListBox>("MessageList");
+            _messageList.TemplateApplied += MessageListOnTemplateApplied;
         }
 
+        private void MessageListOnTemplateApplied(object? sender, TemplateAppliedEventArgs e)
+        {
+            scroll = e.NameScope.Find<ScrollViewer>("PART_ScrollViewer");
+            scroll.ScrollChanged += ScrollOnScrollChanged;          
+        }
+        private void ScrollOnScrollChanged(object? sender, ScrollChangedEventArgs e)
+        {
+           
+        }
+        
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private void BtnScrollToEnd_Click(object sender, RoutedEventArgs e)
+        {
+           scroll.ScrollToEnd();
         }
     }
 }
